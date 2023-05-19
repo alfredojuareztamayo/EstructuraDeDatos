3151965e0e3d3b1cd549d2ab2af811c846f70832
Juan Alfredo <38137911+alfredojuareztamayo@users.noreply.github.com>
2023-05-18 20:27:56 -0600

    Add files via upload

---
+++ ScriptPilas/Nodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila
+{
+    class Nodo
+    {
+        private int dato; 
+        private Nodo next;
+
+
+        public int Dato
+        {
+            get { return dato; }
+            set { dato = value; }   
+        }
+        public Nodo Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
---
+++ ScriptPilas/Pila.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila
+{
+     class Pila
+    {
+        private Nodo Head = new Nodo();
+
+        public Pila() 
+        {
+            Head = null;
+        }
+
+        //Head = 78                        nuevo = 78                          pila datos 23,56,78,89
+
+        //Pila =                      78->  56->   23 -> null
+
+        public void InsertNodo(int num)
+        {
+            Nodo newNodo = new Nodo();
+            newNodo.Dato = num;
+
+            newNodo.Next = Head;
+            Head = newNodo;
+
+           
+        }
+        //Head = 78                        nuevo = 78                          pila datos 23,56,78,89
+
+        //Pila =                      78->  56->   23 -> null
+
+        public void ShowNodo()
+        {
+            Nodo temp = new Nodo();
+            temp = Head;
+            string textNodo = "";
+           
+            if(Head != null)
+            {
+                while(temp != null)
+                {
+ 
+                    textNodo += (temp.Dato.ToString() + "," );
+                    temp = temp.Next;
+                }
+            }
+            else
+            {
+                //debuguear la pila esta vacia gg
+            }
+
+            //Debuguear textNodo
+        }
+    }
+}
